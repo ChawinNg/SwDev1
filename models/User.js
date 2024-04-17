be4f85b974e61f25b75d1f2b92f428cc81b20039
ChawinNg <ik.chawin@gmail.com>
2024-04-18 02:23:43 +0700

    edit enum

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["user", "admin", "publisher"],
+    enum: ["user", "admin"],
     default: "user",
   },
   telephone: {
